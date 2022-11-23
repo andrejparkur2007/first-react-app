14fc77f4967be36b3f4cc2b1ec418d695167cac7
Andrei <115979824+andrejparkur2007@users.noreply.github.com>
2022-11-23 14:54:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    крипыш
+    <br/>
+    <button>Кнопка не для нубов</button>  
+  </div>,
+  document.getElementById('root')
+);
+
